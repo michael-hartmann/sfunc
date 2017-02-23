bb1bfc2929b180d6e27257561c77608cb1e310d1
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-02-23 10:54:32 +0100

    add code to evealue modified Bessel function I at integer order

---
+++ besselI/besselI.h
@@ -0,0 +1,12 @@
+#ifndef __BESSEL_I
+#define __BESSEL_I
+
+double bessel_I0(double x);
+double bessel_I0e(double x);
+
+double bessel_I1(double x);
+double bessel_I1e(double x);
+
+double bessel_I(int n, double x);
+
+#endif
---
+++ besselI/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "besselI.h"
+
+int main(int argc, char *argv[])
+{
+    if(argc < 3)
+    {
+        fprintf(stderr, "%s order argument\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    double x = atof(argv[2]);
+
+    printf("%.15g\n", bessel_I(n,x));
+}
